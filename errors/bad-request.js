5c2439519bdddffbacd2529bc051874aa8abce38
Dan Stronge <dnlstronge@gmail.com>
2023-03-23 10:55:46 +0000

    [x] - create new error messages

---
+++ errors/bad-request.js
@@ -0,0 +1,13 @@
+
+const CustomAPIError = require("./custom-error")
+
+class BadRequestError extends CustomAPIError {
+    constructor(message) {
+      super(message)
+      this.statusCode = 400
+      
+    }
+  }
+  
+  module.exports = BadRequestError
+  
