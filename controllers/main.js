1e4044f1ceff5c28203e400d8485d3d49990cc0e
Dan Stronge <dnlstronge@gmail.com>
2023-03-23 08:04:45 +0000

    [x] - add error handling

--- controllers/main.js
+++ controllers/main.js
@@ -1,8 +1,25 @@
+// only a request with JWT can access the dashboard:
+
+// check for username & password
+// if exists create new JWT
+// send back to frontend
+
+const CustomAPIError = require("../errors/custom-error")
+
 const login = async (req, res) => {
+
+    const { username, password } = req.body
+    if(!username || !password) {
+        throw new CustomAPIError("Please provide email and password", 400)
+    }
+    
     res.send("dummy login/signup route")
 }
 
+
+
 const dashboard = async (req, res) => {
+    
     const luckyNum = Math.floor(Math.random * 100)
     res.status(200).json({msg: `Hello Mr. X`, secret: `Data Authorised: Your lucky number is ${luckyNum}`})
 }
